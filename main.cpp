cda8b789df5d72e5aa6997aed3722ea74805d60c
kynan justis <kfjustis@gmail.com>
2024-02-23 19:07:38 -0600

    Re-organize setup main code and add comments

--- main.cpp
+++ main.cpp
@@ -7,26 +7,28 @@
 #include <iostream>
 
 int main(void) {
+    // Initialize GLFW instance and set up a window.
     glfwInit();
-
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "vkfun", nullptr, nullptr);
 
+    // Query and print Vulkan extensions count for the machine.
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
     std::cout << extensionCount << " extensions supported\n";
 
+    // Verify glm library works.
     glm::mat4 matrix;
     glm::vec4 vec;
     auto test = matrix * vec;
 
+    // Run the window until closed.
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
+    // Clean up window and GLFW instance.
     glfwDestroyWindow(window);
-
     glfwTerminate();
 
     return 0;
